a249bcce857ff5f94699d25440c67ba32462f7fb
Tobenna <tobennandu@gmail.com>
2016-04-12 16:38:28 +0100

    Updating restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'restaurants' do
   context 'no restaurants have been added' do
-    scenario 'should display a prompt to add a resturant' do
+    scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
@@ -22,12 +22,34 @@ feature 'restaurants' do
   end
 
   context 'creating restaurants' do
-    scenario 'prompt user to fill out a form, then display the new restaurant' do
+    scenario 'prompt user to fill out a form; display the new restaurant' do
       visit '/restaurants'
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'KFC'
       click_button 'Create Restaurant'
       expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'viewing restaurants' do
+    let!(:kfc) { Restaurant.create(name: 'KFC') }
+    scenario 'lets a user view a restaurant' do
+      visit '/restaurants'
+      click_link 'KFC'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq "/restaurants/#{kfc.id}"
+    end
+  end
+
+  context 'editing restaurants' do
+    before { Restaurant.create name: 'KFC' }
+    scenario 'let a user edit a restaurant' do      
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
       expect(current_path).to eq '/restaurants'
     end
   end
