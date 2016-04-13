853cc32ad3d95ba85c041a0b6d3f9d52878bd6f8
Sachin Karia <sachin.karia01@gmail.com>
2016-04-13 13:53:27 +0100

    added review and restaurant validations

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -15,4 +15,11 @@ describe Restaurant, type: :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).to_not be_valid
   end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: 'New Resto')
+    restaurant = Restaurant.new(name: 'New Resto')
+    expect(restaurant).to have(1).error_on(:name)
+  end
+
 end
