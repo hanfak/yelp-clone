cc7bcce97c1ecdf6350b4a2203a8819e107b10f1
Tobenna <tobennandu@gmail.com>
2016-04-13 11:57:10 +0100

    Fixed orphaned reviews

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
+  
+  it 'deletes reviews associated with it when deleted' do
+    rest = Restaurant.create name: 'KFC'
+    review  = rest.reviews.create(thoughts: 'Thought', rating: 2)
+    rest.destroy
+    expect(Review.all).to_not include review
+  end
 end
