6c410f4d236e3242e7862f86febf49e2910afece
Tobenna <tobennandu@gmail.com>
2016-04-13 12:25:14 +0100

    Error on name

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ end
 group :test do
   gem 'rspec-rails'
   gem 'capybara'
+  gem 'rspec-collection_matchers'
 end
 
 group :development do
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
-  
+
   it 'deletes reviews associated with it when deleted' do
     rest = Restaurant.create name: 'KFC'
     review  = rest.reviews.create(thoughts: 'Thought', rating: 2)
     rest.destroy
     expect(Review.all).to_not include review
+  end
+
+  it 'is not a valid name if less than 3 characters' do
+    restaurant = Restaurant.new(name: 'kf')
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).to_not be_valid
   end
 end
